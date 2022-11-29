731a9aa8b66dc288ef7472705f97af005e298bb9
MiqiPei <peiminqi@gmail.com>
2022-11-29 10:44:56 -0800

    generate kaptcha

--- community/src/main/java/com/fitme/community/controller/LoginController.java
+++ community/src/main/java/com/fitme/community/controller/LoginController.java
@@ -3,21 +3,35 @@ package com.fitme.community.controller;
 import com.fitme.community.entity.User;
 import com.fitme.community.service.UserService;
 import com.fitme.community.util.CommunityConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -49,17 +63,32 @@ public class LoginController implements CommunityConstant {
     public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
         int result = userService.activation(userId, code);
         if (result == ACTIVATION_SUCCESS) {
-            model.addAttribute("msg", "激活成功,您的账号已经可以正常使用了!");
+            model.addAttribute("msg", "Success! You can login now");
             model.addAttribute("target", "/login");
         } else if (result == ACTIVATION_REPEAT) {
-            model.addAttribute("msg", "无效操作,该账号已经激活过了!");
+            model.addAttribute("msg", "Activation Invalid");
             model.addAttribute("target", "/index");
         } else {
-            model.addAttribute("msg", "激活失败,您提供的激活码不正确!");
+            model.addAttribute("msg", "Activation code incorrect");
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
     }
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
 
+        //place kaptcha into session
+        session.setAttribute("kaptcha",text);
+
+        response.setContentType("img.png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("fail"+e.getMessage());
+        }
+    }
 
 }
